968a63b145491e2a9c1dc46326bfe9b7b7cdb493
deepak-pradhan <deepak.pradhan@gmail.com>
2024-10-12 00:44:49 -0400

    FC.02

---
+++ backend/controllers/base_controller.py
@@ -0,0 +1,157 @@
+from typing import Type, Any, Dict
+
+from pathlib import Path
+
+from fastapi import Depends, APIRouter, Request
+from fastapi.responses import HTMLResponse, JSONResponse
+from fastapi.templating import Jinja2Templates
+from sqlmodel import Session, func, select
+
+from backend.db.databases import get_db
+from backend.models.base import CModel
+from backend.utils.pagination import Pagination
+from backend.utils.filtering import apply_filters
+from backend.utils.sorting import apply_sorting
+
+# FS: Keep this import for FastUI
+# from fastui import FastUI, AnyComponent, prebuilt_html, components as c
+# from fastui.components.display import DisplayMode, DisplayLookup
+# from fastui.events import GoToEvent, BackEvent
+
+class BaseController:
+    """
+    Base controller class providing common functionality for other controllers.
+    """
+    _current_dir: Path = Path(__file__).parent.parent.parent
+    _templates_dir: str = str(_current_dir.joinpath("backend", "templates"))
+
+    def __init__(self, model: Type[CModel], resource_name: str = None, templates_dir: str = None):
+        self.model = model
+        self.templates_dir = templates_dir or self._templates_dir  # Use the provided templates_dir or the default
+
+        if resource_name is None:
+            self.resource_name = self.model.__name__.lower() + 's'
+        else:
+            self.resource_name = resource_name
+
+        self.templates = Jinja2Templates(directory=str(self.templates_dir))
+        
+        self.router = APIRouter()
+
+    def separate_headers_and_data(self, models):
+        if not models:
+            return [],[]
+        
+        model_class= type(models[0])
+
+        # Get the column headers
+        headers = [field.name for field in model_class.__table__.columns]
+        
+        
+        # Extract data rows
+        data_rows = []
+        for instance in models:
+            row = [getattr(instance, field) for field in headers]
+            data_rows.append(row)
+        
+        return headers, data_rows
+
+    # Create
+    async def create(self, request: Request, db: Session = Depends(get_db)):
+        ''' Create a new item '''
+        data = await request.json()
+        model = self.model.from_dict(data)
+        db.add(model)
+        db.commit()
+        db.refresh(model)
+        return
+    async def create_batch(self, request: Request, db: Session = Depends(get_db)):
+        ''' Create a new items '''
+        data = await request.json()
+        models = [self.model.from_dict(item) for item in data]
+        self.model.bulk_create(db, models)
+        return
+    
+    # Read
+    async def read(self, request: Request, id: int, db: Session = Depends(get_db)):
+        model = db.get(self.model, id)
+        if model is None:
+            return JSONResponse(status_code=404, content={"detail": "Item not found"})
+        return model
+    
+    async def read_all(self, request: Request, db: Session = Depends(get_db)):
+        models = db.exec(select(self.model)).all()
+        return models
+        
+    async def list(self
+            , request: Request
+            , db: Session = Depends(get_db)
+            , pagination: Pagination = Depends()
+            , filters: Dict[str, Any] = None
+            , sort: str = None
+        ) -> HTMLResponse:
+        
+        pagination = Pagination(
+            request=Request
+            , page=pagination.page
+            , page_size=pagination.page_size
+        )
+        offset = (pagination.page - 1) * pagination.page_size
+        
+        query = select(self.model)
+        if filters:
+            query = apply_filters(query, filters)
+        
+        if sort:
+            query = apply_sorting(query, sort)
+        
+        models = db.exec(
+            query
+            .offset(offset)
+            .limit(pagination.page_size)
+        ).all()
+
+        # if not models:
+        #     self.logger.info(f"Info: No items in list method")
+        #     return JSONResponse(status_code=404, content={"detail": "Item not found"})
+        headers, data = self.separate_headers_and_data(models)
+        print("\nHeaders:", headers)
+        print("\nData rows:", data)
+
+        total = db.exec(select(func.count(self.model.id))).first()
+
+        return models
+        return self.templates.TemplateResponse(
+                "vendors/list.j2", {
+                        "request": request
+                        , "models": models
+                        , "pagination": pagination
+                        , "total": total
+                    }   
+            )
+        
+    # Update
+    async def update(self, request: Request, id: int, db: Session = Depends(get_db)):
+        data = await request.json()
+        model = db.get(self.model, id)
+        if model is None:
+            return JSONResponse(status_code=404, content={"detail": "Item not found"})
+        
+    # Delete    
+    async def delete(self, request: Request, id: int, db: Session = Depends(get_db)):
+        model = db.get(self.model, id)
+        if model is None:
+            return JSONResponse(status_code=404, content={"detail": "Item not found"})
+        
+        db.delete(model)
+        db.commit()
+        return JSONResponse(content={"detail": "Deleted successfully"})        
+    
+    async def toggle_active(self, id: int, db: Session = Depends(get_db)):
+        model = db.get(self.model, id)
+        if model is None:
+            return JSONResponse(status_code=404, content={"detail": "Item not found"})
+        model.is_active = not model.is_active
+        db.add(model)
+        db.commit()
+        return {"status": "success", "is_active": model.is_active}
---
+++ backend/controllers/vendors_controller.py
@@ -0,0 +1,79 @@
+from datetime import datetime, timezone
+import logging
+
+from fastapi import Depends, Request
+from fastapi.responses import JSONResponse
+from sqlmodel import Session
+
+from backend.db.databases import get_db
+from backend.models.vendor import Vendor
+from backend.controllers.base_controller import BaseController
+
+class VendorsController(BaseController):
+    def __init__(self):
+        super().__init__(Vendor, "vendors")
+        self.register_additional_routes()
+        self.logger = logging.getLogger(__name__)
+
+
+    def register_additional_routes(self):
+        pass
+        self.router.add_api_route(f"/{self.resource_name}", self.create, methods=["POST"], name="create")
+        self.router.add_api_route(f"/{self.resource_name}", self.list, methods=["GET"], name="list")
+        self.router.add_api_route(f"/{self.resource_name}/{{id}}", self.read, methods=["GET"], name="read")
+        self.router.add_api_route(f"/{self.resource_name}/{{id}}", self.update, methods=["PUT"], name="update")
+        self.router.add_api_route(f"/{self.resource_name}/{{id}}", self.delete, methods=["DELETE"], name="delete")
+        # self.router.add_api_route("/vendors/{id}/toggle-active", self.toggle_active, methods=["PUT"], name="toggle_active")
+        # self.router.add_api_route("/vendors/new", self.new_form, methods=["GET"], name="new_form")
+        # self.router.add_api_route("/vendors/{id}/edit", self.edit_form, methods=["GET"], name="edit_form")
+
+    async def create(self, request: Request, db: Session = Depends(get_db)):
+        """
+        Create a new vendor.
+
+        Args:
+            vendor (VendorCreate): The vendor data to create.
+            db (Session): The database session.
+
+        Returns:
+            Vendor: The created vendor object.
+        """
+        form_data = await request.form()
+        model_data = {key: value for key, value in form_data.items() if value}
+        if 'name' not in model_data or not model_data['name']:
+            return JSONResponse(status_code=400, content={"detail": "Name is required"})
+        model = Vendor(**model_data)
+        db.add(model)
+        db.commit()
+        db.refresh(model)
+        return JSONResponse(status_code=200, content={"detail": "Item created"})
+
+    
+    async def update(self, id:int, db: Session = Depends(get_db), **kwargs):
+        """Update the model instance with new values"""
+        # body = await request.json()
+
+        logging.info(f"Updating vendor with ID: {id}")
+        logging.info(f"Received update data: {kwargs}")
+
+        model = db.get(Vendor, id)
+        if not model:
+            logging.warning(f"Vendor with ID {id} not found")
+            return JSONResponse(status_code=404, content={"detail": "Vendor not found"})
+
+        for key, value in kwargs.items():
+            if hasattr(model, key):
+                logging.info(f"Updating field: {key} with value: {value}")
+                setattr(model, key, value)
+            else:
+                logging.warning(f"Attempted to update non-existent field: {key}")
+
+        model.updated_at = datetime.now(timezone.utc)
+        db.add(model)
+        db.commit()
+        db.refresh(model)
+        logging.info(f"Vendor {id} updated successfully")
+        return model
+
+vendors_controller = VendorsController()
+router = vendors_controller.router
